fbe75c89df737023ede90ab172eab17285f48261
ixap2i <ayamashita124@gmail.com>
2021-08-27 16:21:50 +0900

    quagga init

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,43 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Quagga from 'quagga';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted: function() {
+    this.startCamera()
+  },
+  methods: {
+    startCamera: function() {
+      Quagga.init({
+        inputStream: {
+          name: 'Live',
+          type: 'LiveStream',
+          target: document.querySelector('#cameraDemo')
+        },
+        decoder: {
+          readers: ['ean_reader']
+        }
+      },
+      function(err) {
+        if (err) {
+          console.log(err);
+          return
+        }
+        console.log('Initialize was completed.');
+        Quagga.start();
+      })
+      Quagga.onDetected(function(result) {
+        console.log(result.codeResult);
+        if (result.codeResult) {
+          console.log(result.codeResult.code);
+        }
+      });
+    }
   }
 }
 </script>
